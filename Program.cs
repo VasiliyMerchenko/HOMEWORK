81dd8e7f263eb1213aadc36d138b8c35a59f02a5
Vasiliy <vmerchenko@mail.ru>
2024-07-02 22:55:18 +0700

    wrote the code for the 2nd task

--- Program.cs
+++ Program.cs
@@ -3,28 +3,28 @@
 // в промежутке от M до N. Использовать рекурсию, не
 // использовать циклы.
 
-class Program 
-{ 
-    static void Main() 
-    { 
-        Console.Write("Введите значение M: "); 
-        int M = int.Parse(Console.ReadLine()); 
+// class Program 
+// { 
+//     static void Main() 
+//     { 
+//         Console.Write("Введите значение M: "); 
+//         int M = int.Parse(Console.ReadLine()); 
  
-        Console.Write("Введите значение N: "); 
-        int N = int.Parse(Console.ReadLine()); 
+//         Console.Write("Введите значение N: "); 
+//         int N = int.Parse(Console.ReadLine()); 
  
-        PrintNaturalNumbers(M, N); 
-    } 
+//         PrintNaturalNumbers(M, N); 
+//     } 
  
-    static void PrintNaturalNumbers(int current, int N) 
-    { 
-        if (current <= N) 
-        { 
-            Console.Write(current + " "); 
-            PrintNaturalNumbers(current + 1, N); 
-        } 
-    } 
-}
+//     static void PrintNaturalNumbers(int current, int N) 
+//     { 
+//         if (current <= N) 
+//         { 
+//             Console.Write(current + " "); 
+//             PrintNaturalNumbers(current + 1, N); 
+//         } 
+//     } 
+// }
 
 
 
@@ -32,7 +32,30 @@ class Program
 // Аккермана с помощью рекурсии. Даны два
 // неотрицательных числа m и n. 
 
-
+class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите неотрицательное число m: "); 
+        int m = int.Parse(Console.ReadLine()); 
+ 
+        Console.Write("Введите неотрицательное число n: "); 
+        int n = int.Parse(Console.ReadLine()); 
+ 
+        int result = AckermannFunction(m, n); 
+        Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно: {result}"); 
+    } 
+ 
+    static int AckermannFunction(int m, int n) 
+    { 
+        if (m == 0) 
+            return n + 1; 
+        else if (n == 0) 
+            return AckermannFunction(m - 1, 1); 
+        else 
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1)); 
+    } 
+}
 
 
 
